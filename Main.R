4f7aaede4e8ee14085033f98a20480be31e9c3f0
Henrik John <l.john@erasmusmc.nl>
2023-12-04 11:19:56 +0100

    Copy all files from PLP validation module

---
+++ Main.R
@@ -0,0 +1,286 @@
+# Copyright 2023 Observational Health Data Sciences and Informatics
+#
+# This file is part of "DeepPatientLevelPredictionValidationModule
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# function to enable simple GLM to be validated using PLP
+predictGLM <- function(plpModel, data, cohort){
+  require('dplyr')
+  start <- Sys.time()
+  
+  ParallelLogger::logTrace('predictProbabilities using predictGLM')
+  
+  data$covariateData$coefficients <- plpModel$model$coefficients
+  on.exit(data$covariateData$coefficients <- NULL)
+  
+  prediction <- data$covariateData$covariates %>%
+    dplyr::inner_join(data$covariateData$coefficients, by= 'covariateId') %>%
+    dplyr::mutate(values = .data$covariateValue*.data$coefficient) %>%
+    dplyr::group_by(.data$rowId) %>%
+    dplyr::summarise(value = sum(.data$values, na.rm = TRUE)) %>%
+    dplyr::select("rowId", "value")
+  
+  prediction <- as.data.frame(prediction)
+  prediction <- merge(cohort, prediction, by ="rowId", all.x = TRUE, fill = 0)
+  prediction$value[is.na(prediction$value)] <- 0
+  prediction$value <- prediction$value + plpModel$model$intercept
+  
+  # linear/logistic/square/exponential
+  if(plpModel$model$finalMapping == 'linear'){
+    prediction$value <- prediction$value
+  } else if(plpModel$model$finalMapping == 'logistic'){
+    prediction$value <- 1/(1+exp(-prediction$value))
+  } else if(plpModel$model$finalMapping == 'square'){
+    prediction$value <- prediction$value^2
+  } else if(plpModel$model$finalMapping == 'exponential'){
+    prediction$value <- exp(prediction$value)
+  }
+  
+  attr(prediction, "metaData")$modelType <- "binary"
+  
+  delta <- Sys.time() - start
+  ParallelLogger::logInfo("Prediction took ", signif(delta, 3), " ", attr(delta, "units"))
+  return(prediction)
+}
+
+# Module methods -------------------------
+getModuleInfo <- function() {
+  checkmate::assert_file_exists("MetaData.json")
+  return(ParallelLogger::loadSettingsFromJson("MetaData.json"))
+}
+
+getSharedResourceByClassName <- function(sharedResources, className) {
+  returnVal <- NULL
+  for (i in 1:length(sharedResources)) {
+    if (className %in% class(sharedResources[[i]])) {
+      returnVal <- sharedResources[[i]]
+      break
+    }
+  }
+  invisible(returnVal)
+}
+
+# this updates the cohort table details in covariates
+updateCovariates <- function(plpModel, cohortTable, cohortDatabaseSchema){
+  
+  covSettings <- plpModel$modelDesign$covariateSettings
+  # if a single setting make it into a list to force consistency
+  if(inherits(covSettings, 'covariateSettings')){
+    covSettings <- list(covSettings)
+  }
+  
+  for(i in 1:length(covSettings)){
+    if('cohortTable' %in% names(covSettings[[i]])){
+      covSettings[[i]]$cohortTable <- cohortTable
+    }
+    if('cohortDatabaseSchema' %in% names(covSettings[[i]])){
+      covSettings[[i]]$cohortDatabaseSchema <- cohortDatabaseSchema
+    }
+  }
+  
+  plpModel$modelDesign$covariateSettings <- covSettings
+  
+  return(plpModel)
+}
+
+createCohortDefinitionSetFromJobContext <- function(sharedResources, settings) {
+  cohortDefinitions <- list()
+  if (length(sharedResources) <= 0) {
+    stop("No shared resources found")
+  }
+  cohortDefinitionSharedResource <- getSharedResourceByClassName(sharedResources = sharedResources, 
+                                                                 class = "CohortDefinitionSharedResources")
+  if (is.null(cohortDefinitionSharedResource)) {
+    stop("Cohort definition shared resource not found!")
+  }
+  cohortDefinitions <- cohortDefinitionSharedResource$cohortDefinitions
+  if (length(cohortDefinitions) <= 0) {
+    stop("No cohort definitions found")
+  }
+  cohortDefinitionSet <- CohortGenerator::createEmptyCohortDefinitionSet()
+  for (i in 1:length(cohortDefinitions)) {
+    cohortJson <- cohortDefinitions[[i]]$cohortDefinition
+    cohortDefinitionSet <- rbind(cohortDefinitionSet, data.frame(
+      cohortId = as.integer(cohortDefinitions[[i]]$cohortId),
+      cohortName = cohortDefinitions[[i]]$cohortName,
+      json = cohortJson,
+      stringsAsFactors = FALSE
+    ))
+  }
+  return(cohortDefinitionSet)
+}
+
+# Module methods -------------------------
+execute <- function(jobContext) {
+  rlang::inform("Validating inputs")
+  inherits(jobContext, 'list')
+
+  if (is.null(jobContext$settings)) {
+    stop("Analysis settings not found in job context")
+  }
+  if (is.null(jobContext$sharedResources)) {
+    stop("Shared resources not found in job context")
+  }
+  if (is.null(jobContext$moduleExecutionSettings)) {
+    stop("Execution settings not found in job context")
+  }
+  
+  workFolder <- jobContext$moduleExecutionSettings$workSubFolder
+  resultsFolder <- jobContext$moduleExecutionSettings$resultsSubFolder
+  
+  rlang::inform("Executing PLP Validation")
+  moduleInfo <- getModuleInfo()
+
+  # find where cohortDefinitions are as sharedResources is a list
+  cohortDefinitionSet <- createCohortDefinitionSetFromJobContext(
+    sharedResources = jobContext$sharedResources,
+    settings = jobContext$settings
+    )
+  
+  # check the model locations are valid and apply model
+  upperWorkDir <- dirname(workFolder)
+  modelTransferFolder <- sort(dir(upperWorkDir, pattern = 'ModelTransferModule'), decreasing = T)[1]
+  
+  modelSaveLocation <- file.path( upperWorkDir, modelTransferFolder, 'models') # hack to use work folder for model transfer 
+  
+  ParallelLogger::logInfo('Found these models:', paste0(dir(modelSaveLocation), collapse = ', '))
+  modelLocationList <- file.path(modelSaveLocation, dir(modelSaveLocation))
+
+  databaseNames <- c()
+  modelInd <- 0
+  for(modelLocation in modelLocationList){
+    modelInd <- modelInd + 1
+    
+    # ensure model is not a deep learning model
+    modelAttributes <- tryCatch(
+      {jsonlite::fromJSON(file.path(modelLocation, "attributes.json"))},
+      error = function(e){ParallelLogger::logInfo(e); return(NULL)}
+    )
+    
+    if (!is.null(modelAttributes)) {
+      if (modelAttributes$predictionFunction != "predictDeepEstimator") {
+        
+        plpModel <- tryCatch(
+          {PatientLevelPrediction::loadPlpModel(modelLocation)},
+          error = function(e){ParallelLogger::logInfo(e); return(NULL)}
+        )
+        
+        if(!is.null(plpModel)){
+          
+          # append model ind to ensure analysis id is unique
+          plpModel$trainDetails$analysisId <- paste0(plpModel$trainDetails$analysisId, '_', modelInd)
+          
+          # update the plpModel cohort covariates to replace the schema and cohortTable
+          # cohortDatabaseSchema, cohortTable
+          plpModel <- updateCovariates(
+            plpModel = plpModel,
+            cohortTable = jobContext$moduleExecutionSettings$cohortTableNames$cohortTable, 
+            cohortDatabaseSchema = jobContext$moduleExecutionSettings$workDatabaseSchema
+          )
+          
+          # create the database details:
+          databaseDetails <- list()
+          for(ddind in 1:length(jobContext$settings)){
+            
+            tid <- jobContext$settings[[ddind]]$targetId
+            oid <- jobContext$settings[[ddind]]$outcomeId
+            
+            # why not just use database name?
+            databaseNames <- c(databaseNames, paste0(jobContext$moduleExecutionSettings$connectionDetailsReference,'_T',tid,'_O',oid ))
+            
+            databaseDetails[[ddind]] <- PatientLevelPrediction::createDatabaseDetails(
+              connectionDetails = jobContext$moduleExecutionSettings$connectionDetails, 
+              cdmDatabaseSchema = jobContext$moduleExecutionSettings$cdmDatabaseSchema,
+              cohortDatabaseSchema = jobContext$moduleExecutionSettings$workDatabaseSchema,
+              cdmDatabaseName = paste0(jobContext$moduleExecutionSettings$connectionDetailsReference,'_T',tid,'_O',oid ),
+              cdmDatabaseId = jobContext$moduleExecutionSettings$databaseId,
+              #tempEmulationSchema =  , is there s temp schema specified anywhere?
+              cohortTable = jobContext$moduleExecutionSettings$cohortTableNames$cohortTable, 
+              outcomeDatabaseSchema = jobContext$moduleExecutionSettings$workDatabaseSchema, 
+              outcomeTable = jobContext$moduleExecutionSettings$cohortTableNames$cohortTable, 
+              targetId = tid,
+              outcomeIds = oid
+            )
+          }
+          
+          if(!is.null(jobContext$settings[[ddind]]$populationSettings)){
+            ParallelLogger::logInfo('Updating population settings')
+            plpModel$modelDesign$populationSettings <- jobContext$settings[[ddind]]$populationSettings
+          } else{
+            ParallelLogger::logInfo('Using model population settings')
+          }
+          
+          PatientLevelPrediction::externalValidateDbPlp(
+            plpModel = plpModel, 
+            validationDatabaseDetails = databaseDetails, 
+            validationRestrictPlpDataSettings = jobContext$settings[[ddind]]$restrictPlpDataSettings, 
+            settings = jobContext$settings[[ddind]]$validationSettings, 
+            #logSettings = , 
+            outputFolder = workFolder
+          )
+          
+        } else{
+          ParallelLogger::logInfo(paste0('Issue loading model at ', modelLocation))
+        }
+      }
+    }
+  }
+  
+  # save results into sqlite
+  # move results into database
+  for(databaseName in unique(databaseNames)){
+    tryCatch({
+      PatientLevelPrediction::insertResultsToSqlite(
+        resultLocation = file.path(workFolder, databaseName), 
+        cohortDefinitions = cohortDefinitionSet,
+        #databaseList = PatientLevelPrediction::createDatabaseList(
+        #  cdmDatabaseSchemas = validationDatabaseDetail$cdmDatabaseSchema,
+        #  cdmDatabaseNames = validationDatabaseDetail$cdmDatabaseName,
+        #  databaseRefIds = validationDatabaseDetail$cdmDatabaseId 
+        #),
+        sqliteLocation = file.path(workFolder,'sqlite')
+      )
+    })
+  }
+
+  # Export the results
+  rlang::inform("Export data to csv files")
+
+  sqliteConnectionDetails <- DatabaseConnector::createConnectionDetails(
+    dbms = 'sqlite',
+    server = file.path(workFolder, "sqlite","databaseFile.sqlite")
+  )
+    
+  PatientLevelPrediction::extractDatabaseToCsv(
+    connectionDetails = sqliteConnectionDetails, 
+    databaseSchemaSettings = PatientLevelPrediction::createDatabaseSchemaSettings(
+      resultSchema = 'main', # sqlite settings
+      tablePrefix = '', # sqlite settings
+      targetDialect = 'sqlite', 
+      tempEmulationSchema = NULL
+    ), 
+    csvFolder = resultsFolder,
+    fileAppend = NULL
+  )
+  
+  # Zip the results
+  rlang::inform("Zipping csv files")
+  DatabaseConnector::createZipFile(
+    zipFile = file.path(resultsFolder, 'results.zip'),
+    files = file.path(workFolder, 'results')
+  )
+  
+  
+}
---
+++ SettingsFunctions.R
@@ -0,0 +1,37 @@
+createPatientLevelPredictionValidationModuleSpecifications <- function(
+    validationComponentsList = list(
+      list(
+        targetId = 1,
+        oucomeId = 4,
+        restrictPlpDataSettings = PatientLevelPrediction::createRestrictPlpDataSettings(), # vector
+        validationSettings = PatientLevelPrediction::createValidationSettings(
+          recalibrate = "weakRecalibration"
+          ),
+        populationSettings = PatientLevelPrediction::createStudyPopulationSettings(
+          riskWindowStart = 90, 
+          riskWindowEnd = 360,
+          requireTimeAtRisk = F
+          )
+      ), 
+      list(
+        targetId = 3,
+        oucomeId = 4,
+        restrictPlpDataSettings = PatientLevelPrediction::createRestrictPlpDataSettings(), # vector
+        validationSettings = PatientLevelPrediction::createValidationSettings(
+          recalibrate = "weakRecalibration"
+          )
+        
+      )
+    )
+) {
+  
+  specifications <- list(
+    module = "DeepPatientLevelPredictionValidationModule",
+    version = "0.0.1",
+    remoteRepo = "github.com",
+    remoteUsername = "ohdsi",
+    settings = validationComponentsList
+  )
+  class(specifications) <- c("PatientLevelPredictionValidationModuleSpecifications", "ModuleSpecifications")
+  return(specifications)
+}
